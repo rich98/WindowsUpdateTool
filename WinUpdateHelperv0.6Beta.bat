3c93aa11d085130071e28877e660e58ae7025786
rich98 <51236157+rich98@users.noreply.github.com>
2019-09-02 14:44:27 +0100

    Update WinUpdateHelperv0.6Beta.bat

--- WinUpdateHelperv0.6Beta.bat
+++ WinUpdateHelperv0.6Beta.bat
@@ -163,7 +163,7 @@ echo.
 echo  *******************************************************************************************************************
 echo    Version 0.6 Beta Version 				  					            
 echo    No warranty offered or implied 								                    
-echo    License free even for commercial use, to modify, change, you can use in by please give kudos to Richard wadsworth 
+echo    License free even for commercial use, modify, change. But please give kudos to Richard wadsworth 
 echo    Any issues bugs or faults please contact me https://linkedin.com/in/rcwadsworth                                                                
 echo    You use at your own risk. This is a beta version only windows 10 is supported
 echo.                               
